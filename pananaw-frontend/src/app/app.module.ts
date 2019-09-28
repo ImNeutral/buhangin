e7afc9752f4041d9ce0ef706f99425933bdf93d5
Bea Marasigan <mariabeatrizmarasigan@gmail.com>
2019-09-28 11:04:38 +0800

    Add firestore

--- pananaw-frontend/src/app/app.module.ts
+++ pananaw-frontend/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireModule } from 'angularfire2';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule, MatSelectModule, MatInputModule, MatTabsModule } from '@angular/material';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,9 @@ import { MatFormFieldModule, MatSelectModule, MatInputModule, MatTabsModule } fr
     MatFormFieldModule,
     MatSelectModule,
     MatInputModule,
-    MatTabsModule
+    MatTabsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
