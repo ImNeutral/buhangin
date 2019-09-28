75e3c4fa5b08c067288a93daef046987ce24e6dd
clgarillo <cgarillo@ideyatech.com>
2019-09-28 23:05:44 +0800

    Added User model and sample Model mapping for a value.

--- pananaw-frontend/src/app/content/user/user.component.ts
+++ pananaw-frontend/src/app/content/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserModel } from 'src/@shared/model/user.model';
 
 @Component({
   selector: 'app-user',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+
+  user: UserModel = {
+    uid: "123456",
+    name: "Jhon Doe",
+    email: "jhondoe@gmail.com",
+    handler: "@SM",
+    contact: [
+      "janedoe@gmail.com",
+      "brendanshowalter@gmail.com"
+    ]
+  };
 
   constructor() { }
 
